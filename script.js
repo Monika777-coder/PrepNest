94a6ae641c7b672a8358123bd04d090d9c29b692
Harshini S <harshiniis2005@gmail.com>
2024-10-04 19:56:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle dark mode
+const toggleButton = document.getElementById('toggle-mode');
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        toggleButton.textContent = 'Switch to Light Mode';
+    } else {
+        toggleButton.textContent = 'Switch to Dark Mode';
+    }
+});
